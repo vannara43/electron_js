d06aa4af6e5a19c6035d0e56a5b5deb1d2f0e743
Vannara43 <vannarathong@gmail.com>
2025-02-23 15:40:09 -0800

    finished electron basics

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
 function createWindow() {
     let win = new BrowserWindow({
         width: 800,
         height: 600,
     });
     win.loadFile("index.html");
-    win.webContents.openDevTools()
 }
 app.whenReady().then(createWindow);
